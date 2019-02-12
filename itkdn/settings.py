1d2f87256b913d13c5d5fb9e31342c9adb900702
John <pohui_stu@outlook.com>
2019-02-12 22:12:08 +0200

    change

--- itkdn/settings.py
+++ itkdn/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Itproger.urls'
+ROOT_URLCONF = 'itkdn.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Itproger.wsgi.application'
+WSGI_APPLICATION = 'itkdn.wsgi.application'
 
 
 # Database
